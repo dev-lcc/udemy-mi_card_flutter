4c61a945caccc6c576081ec24083b885e0206ca0
Lawrence C <52389524+rsvp-lawrencec@users.noreply.github.com>
2022-04-24 17:53:34 +0800

    Challenge completed!

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,56 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+      color: Colors.teal,
+      home: LayoutChallengePage(),
+    )
   );
+}
+
+class LayoutChallengePage extends StatelessWidget {
+  const LayoutChallengePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Center(
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100.0,
+                        height: 100.0,
+                        color: Colors.yellow,
+                      ),
+                      Container(
+                        width: 100.0,
+                        height: 100.0,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
